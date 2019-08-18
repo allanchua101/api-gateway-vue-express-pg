c3c7ed3596f63cb5a820ad745f5eda2b60bc8b5b
Allan Chua <allanchua.officefiles@gmail.com>
2019-08-19 02:24:55 +0800

    -Updated Gateway API
    - Updated runtime version to latest alpine distribution
    - Introduced healthcheck
    - Removed values controller and introduced hello controller

--- gateway/WebGateway/Startup.cs
+++ gateway/WebGateway/Startup.cs
@@ -34,17 +34,6 @@ namespace WebGateway
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Use(async (context, next) =>
-            {
-                if (context.Request.Path == "/")
-                {
-                    context.Response.Redirect("/#/", permanent: false);
-                    return;
-                }
-
-                await next();
-            });
-
             app.UseMvc();
         }
     }
