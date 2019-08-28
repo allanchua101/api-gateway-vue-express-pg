09d4efececcc91e8bdb8579b48c7dd37012ae1ef
Allan Chua <allanchua.officefiles@gmail.com>
2019-08-29 00:34:15 +0800

    - Implemented passing of AWS profile to be utilized when validating CF templates

--- scripts/quality-gates/001_validate-cf-templates.sh
+++ scripts/quality-gates/001_validate-cf-templates.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
-ERROR_COUNT=0; 
+ERROR_COUNT=0;
+PROFILE_NAME=$1;
+
+if [ -z "$PROFILE_NAME" ]
+then
+      echo "\$PROFILE_NAME is empty! Stopping console in 3 seconds.."
+      echo ""
+      echo ""
+      read -p "Press any key to exit...." key
+      exit 1;
+else
+      echo "Validating templates using profile named $PROFILE_NAME"
+      echo ""
+      echo ""
+fi
 
 echo "Validating AWS CloudFormation templates..."
+echo ""
+echo ""
 
-# Loop through the YAML templates in this repository
-for TEMPLATE in $(find . -name '*.yaml'); do 
-
+# Loop through the YAML templates in the infrastructure folder.
+for TEMPLATE in $(find ../infrastructure -name '*.yaml'); do 
     # Validate the template with CloudFormation
-    ERRORS=$(aws cloudformation validate-template --template-body file://$TEMPLATE 2>&1 >/dev/null); 
+    ERRORS=$(aws cloudformation validate-template --profile $PROFILE_NAME --template-body file://$TEMPLATE 2>&1 >/dev/null); 
     if [ "$?" -gt "0" ]; then 
         ((ERROR_COUNT++));
         echo "[fail] $TEMPLATE: $ERRORS";
@@ -18,6 +33,12 @@ for TEMPLATE in $(find . -name '*.yaml'); do
 done; 
 
 echo "$ERROR_COUNT template validation error(s)"; 
+echo ""
+echo ""
+
+# Uncomment this line if you want to exit
+# read -p "Press any key to exit..." exitkey
+
 if [ "$ERROR_COUNT" -gt 0 ]; 
     then exit 1; 
 fi
