c32663682853332c54dac0e6c8cfa0e6382d29e9
Allan Chua <allanchua.officefiles@gmail.com>
2019-09-15 15:16:52 +0800

    -Included ecr creation scripts

--- scripts/001_release-base-sample.sh
+++ scripts/001_release-base-sample.sh
@@ -8,9 +8,15 @@ PROFILE_NAME="";
 # as a storage for your cloud formation templates.
 TEMPLATE_STORE_BUCKET_NAME="";
 
+# Variable containing name of ECR.
+ECR_NAME="";
+
 # Variable containing the name of your cloudformation
 # stack name.
 TEMPLATE_STORE_STACK_NAME="template-store-stack";
+
+# Variable containing name of ECR.
+ECR_STACK_NAME="ecr-stack";
 
 # Variable containing path of infrastructure CF templates.
 INFRASTRUCTURE_FILES=./infrastructure/*.yaml
@@ -29,6 +35,14 @@ fi
 if [ -z "$TEMPLATE_STORE_BUCKET_NAME" ]
 then
   echo "Please set the value of TEMPLATE_STORE_BUCKET_NAME variable inside the script."
+  echo "Press any key to exit...."
+  read
+  exit 0
+fi
+
+if [ -z "$ECR_NAME" ]
+then
+  echo "Please set the value of ECR_NAME variable inside the script."
   echo "Press any key to exit...."
   read
   exit 0
@@ -70,10 +84,30 @@ do
   echo "Uploading $f file..."
   aws s3 cp $f "s3://${TEMPLATE_STORE_BUCKET_NAME}/${f/.\//$''}" --profile "${PROFILE_NAME}" &> /dev/null
 done
+echo "================================================"
+
+# Ensure template storage exists.
+if ! aws cloudformation describe-stacks --profile "$PROFILE_NAME" --stack-name "$ECR_STACK_NAME" &> /dev/null ; then
+  echo "Creating ECR cloudformation stack.."
+  aws cloudformation create-stack \
+    --profile "${PROFILE_NAME}" \
+    --stack-name "${ECR_STACK_NAME}" \
+    --template-body file://pipeline/ecr.yaml \
+    --parameters ParameterKey=EcrName,ParameterValue="${ECR_NAME}" &> /dev/null
+
+  echo "Waiting ECR cloudformation stack.."
+  aws cloudformation wait stack-create-complete \
+    --profile "${PROFILE_NAME}" \
+    --stack-name "${ECR_STACK_NAME}"
+
+  echo "ECR Cloudformation stack created..."
+else
+  echo "ECR Cloudformation already exists..."
+fi
 
 echo "================================================"
 echo "Press any key to exit..."
 echo "================================================"
 
 # If you are debugging, uncomment read command below
-# read
+read
